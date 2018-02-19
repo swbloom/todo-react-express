1ba80a8dbc39aacf42f47f8144d9334823918feb
Simon Bloom <swbloom@gmail.com>
2018-02-19 10:52:53 -0500

    initial structure

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 8080;
+const todos = require('./lib/todos');
+
+const app = express();
+
+app.use('/todos', todos);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
